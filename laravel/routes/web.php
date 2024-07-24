3f6885f474e1033b241db07ec41466c9be2e68d5
josepedrolorenzini <joseplorenzini@gmail.com>
2024-07-25 07:59:23 +1000

    changes laravel

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    //return "about page";
+    return ['json' => 'about'] ;
 });
