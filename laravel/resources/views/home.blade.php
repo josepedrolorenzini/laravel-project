029d0264772563ceb3abfc7f0f135271b08611e8
josepedrolorenzini <joseplorenzini@gmail.com>
2024-07-25 08:55:49 +1000

    working on the templates

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,1 +1,10 @@
-<p>hello world</p>
+@extends("app")
+
+
+
+
+@section('content')
+<x-products></x-products>
+<a href="https://learninglaravel.net/cheatsheet/#routing">route laravel</a>
+<a href="https://learninglaravel.net/cheatsheet">learn laravel</a>
+@endsection('content')
